c885ac6505081441eb89f867b23cb746e3364403
Hildebrando Escudero <hescudero@cloudtask.com>
2021-10-13 13:29:03 -0400

    added mongoose schema

---
+++ src/items/schemas/item.schema.ts
@@ -0,0 +1,28 @@
+// //Old way
+// import * as mongoose from 'mongoose';
+
+
+// export const ItemSchema = new mongoose.Schema({
+//     name:String,
+//     qty: Number,
+//     description: String
+// })
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ItemDocument = Item & Document;
+
+@Schema()
+export class Item {
+  @Prop()
+  name: string;
+
+  @Prop()
+  age: number;
+
+  @Prop()
+  description: string;
+}
+
+export const ItemSchema = SchemaFactory.createForClass(Item);
