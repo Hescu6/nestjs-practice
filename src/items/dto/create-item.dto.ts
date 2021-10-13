506ba3fe82a081100ddc954b277999bde5a40acf
Hildebrando Escudero <hescudero@cloudtask.com>
2021-10-13 11:46:04 -0400

    added Post, put, delete examples with/without params examples

---
+++ src/items/dto/create-item.dto.ts
@@ -0,0 +1,9 @@
+export class CreateItemDto {
+    readonly name: string;
+    readonly description: string;
+    readonly qty: number;
+}
+
+    /*  Create a DTO(data transfer object) schema
+        defines how the data will be send over the network
+    */
--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,9 +1,46 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Req, Res, Param } from '@nestjs/common';
+import { CreateItemDto } from './dto/create-item.dto';
+import { Request, Response } from 'express';
+import { identity } from 'rxjs';
 
-@Controller('items')
+@Controller('items')    //name of route
 export class ItemsController {
-    @Get()
-    findAll():string {
-        return 'Get All Items';
+
+
+    @Get()              //decorator for get, post, delete, put functions
+    /* YOU CAN USE HE req/res as in express but it's not the nestjs way */
+    findAll(@Req() req: Request, @Res() res:Response):Response {  //name of the function 
+
+        console.log(req.url);
+        return res.send('Hellow Works');
+        // return 'Get All Items'; //response
     }
+
+    @Get(':id')
+    findOne(@Param() param): string {
+        return `Item - ${param.id}`
+    }
+
+
+
+    /*  Create a DTO(data transfer object) schema
+        defines how the data will be send over the network
+    */
+    @Post()
+    create(@Body() createItemDto: CreateItemDto):string {
+        return `Name: ${createItemDto.name}
+                Description: ${createItemDto.description}
+                `;
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id:string){
+        return `Delete ${id}`;
+    }
+
+    @Put(':id')
+    update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
+        return `Update ${id} - Name: ${updateItemDto.name}`;
+    }
+
 }
