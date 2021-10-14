6423600a93063b62d3c49e8bb9fa43a364ffd96d
Hildebrando Escudero <hescudero@cloudtask.com>
2021-10-14 09:51:05 -0400

    added functions to perform CRUD operations on MongoDB

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -36,20 +36,18 @@ export class ItemsController {
         defines how the data will be send over the network
     */
     @Post()
-    create(@Body() createItemDto: CreateItemDto):string {
-        return `Name: ${createItemDto.name}
-                Description: ${createItemDto.description}
-                `;
+    create(@Body() createItemDto: CreateItemDto):Promise<Item> {
+        return this.itemsService.create(createItemDto);
     }
 
     @Delete(':id')
     delete(@Param('id') id:string){
-        return `Delete ${id}`;
+        return this.itemsService.delete(id);
     }
 
     @Put(':id')
-    update(@Body() updateItemDto: CreateItemDto, @Param('id') id): string {
-        return `Update ${id} - Name: ${updateItemDto.name}`;
+    update(@Body() updateItemDto: CreateItemDto, @Param('id') id): Promise<Item> {
+        return this.itemsService.update(id, updateItemDto);
     }
 
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -3,6 +3,7 @@ import { Item } from './interfaces/item.interface';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 
+
 @Injectable()
 export class ItemsService {
 
@@ -27,9 +28,7 @@ export class ItemsService {
     //     },
     // ];
 
-    constructor (@InjectModel('Item') private readonly itemModel:Model<Item>){
-
-    }
+    constructor (@InjectModel('Item') private readonly itemModel:Model<Item>){}
 
     async findAll(): Promise<Item[]> {
         return await this.itemModel.find();
@@ -38,4 +37,19 @@ export class ItemsService {
     async findOne(id: string): Promise<Item> {
         return await this.itemModel.findOne({_id: id});
     }
+
+    async create(item:Item): Promise<Item> {
+        const newItem = new this.itemModel(item);
+        return await newItem.save();
+    }
+
+    async delete(id: string): Promise<Item> {
+        return await this.itemModel.findByIdAndRemove(id)
+    }
+
+    async update(id: string, item: Item): Promise<Item> {
+        return await this.itemModel.findByIdAndUpdate(id, item, {new: true});
+    }
+
+
 }
